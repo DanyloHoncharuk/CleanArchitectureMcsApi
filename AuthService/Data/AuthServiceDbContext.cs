0faea7440758ec85e70507d8f85b090ff0128f23
DHNK <grindewaldy@gmail.com>
2025-03-11 21:06:27 +0200

    Add project files.

---
+++ AuthService/Data/AuthServiceDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AuthService.Models;
+
+namespace AuthService.Data
+{
+    public class AuthServiceDbContext(DbContextOptions<AuthServiceDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+        }
+    }
+}
---
+++ AuthService/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Models
+{
+    public class User
+    {
+        [Key]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string PasswordHash { get; set; }
+
+        [EmailAddress]
+        [MaxLength (100)]
+        public string? EmailAddress { get; set; } = null;
+
+        [Required]
+        public DateTime CreationDate { get; set; }
+        public DateTime? UpdateDate { get; set; } = null;
+        public bool IsActive { get; set; } = true;
+        public bool IsDeleted { get; set; } = false;
+
+    }
+}
