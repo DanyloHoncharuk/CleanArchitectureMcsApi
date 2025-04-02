0a16d707e5305e2a5b5b6dc4d76841062f317285
DanyloHoncharuk <d.ol.honcharuk@gmail.com>
2025-04-02 21:07:08 +0300

    Revert "User mcs creation (base part)"

--- UserService/Application/Interfaces/IDbContextTransactionManager.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UserService.Application.Interfaces
-{
-    public interface IDbContextTransactionManager
-    {
-        Task SaveChangesAsync();
-    }
-}
--- UserService/Common/DateFormats.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserService.Common
-{
-    public class DateFormats
-    {
-        public const string DateOnly = "yyyy-MM-dd";
-        public const string DateTimeWithMilliseconds = "yyyy-MM-dd hh:mm:ss.sss";
-    }
-}
