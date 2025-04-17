3c00eb25636148ba33d4210376225809699a573e
DHNK <grindewaldy@gmail.com>
2025-04-17 21:10:17 +0300

    Add swagger doc agregation on gateway level

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -3,7 +3,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("https://localhost:7051/swagger/v1/swagger.json", "AuthService - 1.0");
+    options.SwaggerEndpoint("https://localhost:7035/swagger/v1/swagger.json", "UserService - 1.0");
+});
 
 app.MapReverseProxy();
 
